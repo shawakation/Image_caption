856470abc83630573e58d424fd9d7dfaee446c80
hahaha <123@123.com>
2020-03-28 19:00:06 +0800

    f

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from torch.nn.utils.rnn import pack_padded_sequence
 from data_loader import *
 from models import EncoderCNN, DecoderRNN
 
-device = torch.device('cuda:0')
+device = torch.device('cuda:2')
 
 
 def do(args: argparse.Namespace):
@@ -74,11 +74,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # load and train params
     parser.add_argument('--random_crop_size', type=int, default=224, help='size of randomly cropping images')
-    parser.add_argument('--save_model_path', type=str, default='.\\checkpoints', help='path to save models when training')
-    parser.add_argument('--dataset_path', type=str, default='.\\data\\train2014_resize', help='path for coco train dataset resized')
-    parser.add_argument('--json_path', type=str, default='.\\data\\annotations\\captions_train2014.json', help='path for caption\'s json path')
-    parser.add_argument('--vocab_path', type=str, default='.\\data\\vocab.pickle', help='path for vocab.pickle')
-    parser.add_argument('--batch_size', type=int, default=12, help='batch size')
+    parser.add_argument('--save_model_path', type=str, default='checkpoints', help='path to save models when training')
+    parser.add_argument('--dataset_path', type=str, default='data/train2014_resize', help='path for coco train dataset resized')
+    parser.add_argument('--json_path', type=str, default='data/annotations/captions_train2014.json', help='path for caption\'s json path')
+    parser.add_argument('--vocab_path', type=str, default='data/vocab.pickle', help='path for vocab.pickle')
+    parser.add_argument('--batch_size', type=int, default=65, help='batch size')
     parser.add_argument('--print_step', type=int, default=10, help='step size for prining log info')
     parser.add_argument('--save_model_step', type=int, default=1000, help='step size for saving trained models')
     # model params
